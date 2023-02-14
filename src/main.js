cdfc0f567b53742f9c062534ea0e6d057eab0cec
Marcos Vinícius Rodrigues Pires de Lima <mvrpl.me@gmail.com>
2023-02-13 23:41:12 -0300

    Added tz info

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { PuppeteerCrawler } from 'crawlee';
 import { router } from './routes.js';
 import { MongoClient } from 'mongodb';
 import moment from 'moment';
+import 'moment-timezone';
 import { argv } from 'node:process';
 
 const crawler = new PuppeteerCrawler({
@@ -9,7 +10,7 @@ const crawler = new PuppeteerCrawler({
     headless: true
 });
 
-global.dtNow = moment().subtract(3, 'hours');
+global.dtNow = moment().tz('America/Sao_Paulo').format();
 
 const mongoURI = argv[2];
 const mercadoID = parseInt(argv[3]) || null;
